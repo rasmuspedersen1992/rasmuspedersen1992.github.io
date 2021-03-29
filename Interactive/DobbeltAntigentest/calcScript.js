1bae424525e18912ae37ed8cf037a1356ed48aa7
Rasmus Kristoffer Pedersen <rasmuspedersen1992@gmail.com>
2021-03-29 13:51:59 +0200

    Added comments and cleaned up more

--- Interactive/DobbeltAntigentest/calcScript.js
+++ Interactive/DobbeltAntigentest/calcScript.js
@@ -1,11 +1,13 @@
-let test;
+// --- Initialize variables ---
+// - Inputs -
 let prae;
+let test;
 let sens;
 let spec;
 
 let prae_scaled;
 
-
+// - Calculated variables -
 let pProb;
 let pPV;
 let ppProb;
@@ -14,6 +16,7 @@ let pnProb;
 let pnPV;
 let pnPV_prae;
 
+// - Size of groups -
 let posTest;
 let opspor;
 let sandPos;
@@ -22,6 +25,7 @@ let isoVent;
 let senSpor;
 let ratioDelay;
 
+// --- Get reference to html-things --
 let test_slider = document.getElementById('test_slider');
 let prae_slider = document.getElementById('prae_slider');
 let sens_slider = document.getElementById('sens_slider');
@@ -32,7 +36,7 @@ let prae_label = document.getElementById('prae_label');
 let sens_label = document.getElementById('sens_label');
 let spec_label = document.getElementById('spec_label');
 
-
+// Function for getting the values from the sliders
 function getAllValues(){
     prae = prae_slider.value;
     sens = sens_slider.value;
@@ -40,32 +44,32 @@ function getAllValues(){
     test = test_slider.value;
 }
 
+// Function for setting the labels below the sliders
 function updateAllLabels(){
+    prae_label.innerHTML ='Prævalens i testpopulation: ' + (prae/100) + '%';
     test_label.innerHTML ='Antal Tests: '+ test;
-    // prae_label.innerHTML ='Prævalens: ' + (prae/100) + '%';
-    prae_label.innerHTML ='Prævalens i testgruppe: ' + (prae/100) + '%';
-    // prae_label.innerHTML ='Testgruppeprævalens: ' + (prae/100) + '%';
     let sensToShow = (sens*10000)/100; // To round off at two decimals
-    sens_label.innerHTML ='Sensitivitet: '+ (sensToShow) + '%';
     let specToShow = (spec*10000)/100; // To round off at two decimals
+    sens_label.innerHTML ='Sensitivitet: '+ (sensToShow) + '%';
     spec_label.innerHTML ='Specificitet: '+ specToShow + '%';
     
 }
+
+// --- onchange functions for sliders ---
 
 test_slider.onchange = function(){
     // Get value
     test = test_slider.value;
 
-    // Update display
+    // Update everything
     updateAll();
 }
-
 
 prae_slider.onchange = function(){
     // Get value
     prae = prae_slider.value;
 
-    // Update display
+    // Update everything
     updateAll();
 }
 
@@ -73,7 +77,7 @@ sens_slider.onchange = function(){
     // Get value
     sens = sens_slider.value;
 
-    // Update display
+    // Update everything
     updateAll();
 }
 
@@ -81,11 +85,16 @@ spec_slider.onchange = function(){
     // Get value
     spec = spec_slider.value;
 
-    // Update display
+    // Update everything
     updateAll();
 }
 
+// Function for doing all calculations
 function updateAll(){    
+    
+    // Get all values, just to be sure
+    getAllValues();
+
     prae_scaled = prae / 10000;
 
     pProb = prae_scaled * sens + ((1-prae_scaled)*(1-spec));
@@ -102,25 +111,22 @@ function updateAll(){
     stopSpor = opspor - sandPos;
     isoVent = pnProb * test;
     senSpor = isoVent * pnPV;
-    // If divide by zero, just show 0
+    // If divide by zero, just use 0
     if (isNaN(senSpor)){
         senSpor = 0;
     }
 
     ratioDelay = senSpor / (sandPos + senSpor);
 
-    
-    getAllValues();
+    // Update all labels
     updateAllLabels();
 }
 
-
-
-getAllValues();
+// Run updateAll initialy
 updateAll();
-updateAllLabels();
 
-// Visual flowchart
+// --- Visual flowchart ---
+// - Initialize variables for colors -
 let clrBackground;
 let clrBox;
 let clrBoxStroke;
@@ -136,14 +142,17 @@ let clrIsoStroke;
 let clrSpor;
 let clrSporStroke;
 
+// Box sizes
 let boxH = 100;
 let boxW = 150;
 
+// Sizes for plus and minus symbol 
 let conSize = 5;
 let symbolSize = 6;
 let circSize = symbolSize*4; 
 let symbolWeight = 2.5;
 
+// Define boxes 
 class GroupBox {
     constructor(x,y,type){
         this.x = x;
@@ -216,6 +225,7 @@ class GroupBox {
     }
 }
 
+// Define arrow-connectors
 class Connector {
     constructor(box1,box2,res,ori,clr,typ){
         this.box1 = box1;
@@ -227,25 +237,29 @@ class Connector {
     }
 
     draw(){
-        // translate(this.box1.x,this.box1.y);
         
         stroke(this.clr);
         strokeWeight(conSize);
 
+        // If the orientation is downwards
         if (this.ori == 'd'){
+            // Draw arrow
             line(this.box1.x+boxW/2,this.box1.y+boxH,this.box2.x+boxW/2,this.box2.y);
             line(this.box2.x+boxW/2,this.box2.y,this.box2.x+boxW/2-conSize,this.box2.y-conSize);
             line(this.box2.x+boxW/2,this.box2.y,this.box2.x+boxW/2+conSize,this.box2.y-conSize);
-            // Show test result
+
+            // Show test a symbol for result
             if (this.res == 'p'){
                 push()
                 translate(this.box1.x + boxW/2, this.box1.y + boxH); // Bottom, start of arrow
 
+                // Draw circle 
                 translate(symbolSize,symbolSize); // Offset
                 strokeWeight(symbolWeight/2);
                 noFill();
                 circle(circSize/2,circSize/2,circSize)
 
+                // Draw plus-sign
                 translate(symbolSize*2,symbolSize*2); // Offset
                 strokeWeight(symbolWeight/2);
                 line(-symbolSize,0,symbolSize,0);
@@ -253,10 +267,10 @@ class Connector {
 
                 pop();
 
+                // For the PCR-test, write delay text
                 if (this.typ == 'PCR'){
                     push()
                     noStroke()
-                    // fill(this.clr);
                     textSize(10);
                     textStyle(ITALIC)
                     textAlign(LEFT)
@@ -265,9 +279,9 @@ class Connector {
                     text('24 timer',0,0)
                     pop();
                 }
-                
             } 
-        } else if (this.ori == 'r'){
+        } else if (this.ori == 'r'){ // If the direction of the arrow is to the right     
+            // Draw arrow       
             line(this.box1.x+boxW,this.box1.y+boxH/2,this.box2.x,this.box2.y+boxH/2);
             line(this.box2.x,this.box2.y+boxH/2,this.box2.x-conSize,this.box2.y+boxH/2-conSize);
             line(this.box2.x,this.box2.y+boxH/2,this.box2.x-conSize,this.box2.y+boxH/2+conSize);
@@ -276,11 +290,13 @@ class Connector {
                 push()
                 translate(this.box1.x + boxW, this.box1.y + boxH/2); // Right hand side, start of arrow
 
+                // Draw circle
                 translate(symbolSize,-symbolSize-circSize); // Offset
                 strokeWeight(symbolWeight/2);
                 noFill();
                 circle(circSize/2,circSize/2,circSize)
 
+                // Draw minus arrow
                 translate(symbolSize*2,-symbolSize*2+circSize); // Offset
                 strokeWeight(symbolWeight);
                 line(-symbolSize,0,symbolSize,0);
@@ -288,10 +304,10 @@ class Connector {
                 // point(0,symbolSize);
                 pop();
 
+                // For the PCR-test, write delay text
                 if (this.typ == 'PCR'){
                     push();
                     noStroke()
-                    // fill(this.clr);
                     textSize(10);
                     textStyle(ITALIC)
                     textAlign(RIGHT)
@@ -300,39 +316,12 @@ class Connector {
                     text('24 timer',0,0)
                     pop();
                 }
-                
-            } 
-            
-        } else if (this.ori == 'l'){
-            line(this.box1.x,this.box1.y+boxH/2,this.box2.x + boxW,this.box2.y+boxH/2);
-            line(this.box2.x + boxW,this.box2.y+boxH/2,this.box2.x + boxW+conSize,this.box2.y+boxH/2-conSize);
-            line(this.box2.x + boxW,this.box2.y+boxH/2,this.box2.x + boxW+conSize,this.box2.y+boxH/2+conSize);
-            // Show test result
-            if (this.res == 'n'){
-                push()
-                translate(this.box1.x, this.box1.y + boxH/2); // Right hand side, start of arrow
-                translate(-symbolSize*2,-symbolSize*2); // Offset
-                line(-symbolSize,0,symbolSize,0);
-                point(0,-symbolSize);
-                point(0,symbolSize);
-                
-                pop();
             } 
         }
-
     }
 }
 
-
-function drawConnect(x,y){
-    fill(clrBox);
-    noStroke();
-    rect(x,y,boxW,boxH);
-}
-
-// let boxTest;
-// let boxPos1;
-
+// - Initialize placement variables -
 let curLeft = 50;
 let curTop = 50;
 let curH = boxH*1.6;
@@ -340,10 +329,16 @@ let curW = boxW*1.4;
 
 let allp5Objects = [];
 
+// - Main p5 setup -
 function setup() {
     var curCanves = createCanvas(650,  520);
     curCanves.parent("p5Div");
     
+    // Set default textstyle
+    textAlign(CENTER,CENTER);
+    textSize(14);
+    
+    // Set colors
     clrBackground = color(255);
     clrBox = color(250,230,230);
     clrBoxStroke = color(230,140,140)
@@ -359,8 +354,6 @@ function setup() {
     clrSpor = color(220,220,255,200);
     clrSporStroke = color(180,180,200,220);
 
-    textAlign(CENTER,CENTER);
-    textSize(14);
 
     // Define boxes and position of them
     boxTest = new GroupBox(curLeft,curTop,'ini');
@@ -369,6 +362,7 @@ function setup() {
     boxIsol = new GroupBox(curLeft + curW,curTop + curH,'pn');
     boxLate  = new GroupBox(curLeft + curW,curTop + curH * 2,'pnP');
 
+    // Add to array to be drawn
     allp5Objects.push(boxTest);
     allp5Objects.push(boxPos1);
     allp5Objects.push(boxPos2);
@@ -379,10 +373,11 @@ function setup() {
     boxNeg1 = new GroupBox(curLeft + curW,curTop,'n');
     boxNeg2 = new GroupBox(curLeft + curW * 2,curTop + curH * 1,'pnN');
 
+    // Add to array to be drawn
     allp5Objects.push(boxNeg1);
     allp5Objects.push(boxNeg2);
 
-    // Define connector
+    // Define connectors
     let conP1 = new Connector(boxTest,boxPos1,'p','d',clrAg1_P,'AG');
     let conP2 = new Connector(boxPos1,boxPos2,'p','d',clrAg2_P,'AG');
     let conLate=new Connector(boxIsol,boxLate,'p','d',clrPCR_P,'PCR');
@@ -390,6 +385,7 @@ function setup() {
     let conN2 = new Connector(boxPos1,boxIsol,'n','r',clrAg2_N,'AG');
     let conN3 = new Connector(boxIsol,boxNeg2,'n','r',clrPCR_N,'PCR');
 
+    // Add to array to be drawn
     allp5Objects.push(conP1);
     allp5Objects.push(conP2);
     allp5Objects.push(conLate);
@@ -399,59 +395,66 @@ function setup() {
 
 }
 
+// - Main p5 draw function -
 function draw() {
 
+    // Draw the background
     background(clrBackground);
-    textAlign(CENTER,CENTER);
 
+    // Set default textstyle
+    textAlign(CENTER,CENTER);
+    textSize(14);
+    
+    // Draw isolation box
     fill(clrIso);
     stroke(clrIsoStroke);
     rect(curLeft-boxW*0.3,curTop+curH*0.9,boxW*0.35+curW*1.9,curH*1.9)
     
+    // Write isolation text
     push();
     translate(curLeft-boxW*0.2,curTop+curH*(0.9+0.4));
     fill(clrText);
     stroke(clrText);
     strokeWeight(0.25);
-    // noStroke()
     textStyle(BOLD)
     rotate(-PI/2);
     text('Isolation',0,0);
     pop();
 
+    // Draw contact-tracing box
     fill(clrSpor);
     stroke(clrSporStroke);
     rect(curLeft-boxW*0.225,curTop+curH*1.9,boxW*0.2+curW*1.9,curH*0.82)
 
-    push();
-    
+    // Write smittesporing text
+    push();    
     translate(curLeft-boxW*0.125,curTop+curH*2.3);
     fill(clrText);
     stroke(clrText);
     strokeWeight(0.25);
-    
     textStyle(BOLD)
     rotate(-PI/2);
     text('Smitteopsporing',0,0);
     pop();
 
+    // Draw all objects
     for (let k = 0; k < allp5Objects.length; k++) {
         allp5Objects[k].draw();        
     }
 
-    // For legend
+    // -- Legend --
     let lgdW = boxW/3;
     let lgdH = boxH/3;
     let lgdTop = height - lgdH * 5.2;
     let lgdLeft = width - lgdW * 2;
+    let lgdMargin = 20;
 
     textAlign(LEFT,BOTTOM);
     textSize(12);
 
     push()
+    // Top label
     translate(lgdLeft,lgdTop);
-
-    let lgdMargin = 20;
     fill(230);
     stroke(220);
     rect(-lgdMargin,-lgdMargin,lgdW+lgdMargin*2,lgdH*4 + lgdMargin*2);
@@ -460,8 +463,10 @@ function draw() {
     textStyle(BOLD);
     text('Forklaring',-2,0);
     
+    // Normal, not bold, text
     textStyle(NORMAL);
     
+    // Ag positve
     translate(0,lgdH);
     fill(clrAg1_P);
     stroke(clrAg1_P);
@@ -472,6 +477,7 @@ function draw() {
     fill(0);
     text('Ag+',0,-5);
     
+    // Ag negative
     translate(0,lgdH);
     fill(clrAg1_N);
     stroke(clrAg1_N);
@@ -482,7 +488,7 @@ function draw() {
     fill(0);
     text('Ag-',0,-5);
     
-    
+    // PCR positive
     translate(0,lgdH);
     fill(clrPCR_P);
     stroke(clrPCR_P);
@@ -493,7 +499,7 @@ function draw() {
     fill(0);
     text('PCR+',0,-5);
     
-    
+    // PCR negative
     translate(0,lgdH);
     fill(clrPCR_N);
     stroke(clrPCR_N);
@@ -504,7 +510,5 @@ function draw() {
     fill(0);
     text('PCR-',0,-5);
 
-    pop()
-
-
+    pop() // - End legend -
 }
